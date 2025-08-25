e3fb3a49a2bb235324f48b9a4de3c08798cbd812
Kiattisak Changthong <hgfdd8230@gmail.com>
2025-08-25 11:48:34 +0700

    Updated about split files to make them look neat.

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../components/expandable_info_card.dart'; // import ไฟล์ที่แยก
 
-class AboutScreen extends StatefulWidget {
+class AboutScreen extends StatelessWidget {
   const AboutScreen({Key? key}) : super(key: key);
-
-  @override
-  State<AboutScreen> createState() => _AboutScreenState();
-}
-
-class _AboutScreenState extends State<AboutScreen> {
-  bool _isAppInfoExpanded = false;
-  bool _isDeveloperExpanded = false;
-  bool _isContactExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -96,50 +88,29 @@ class _AboutScreenState extends State<AboutScreen> {
             ),
             const SizedBox(height: 40),
 
-            // Expandable About Content
-            _buildExpandableInfoCard(
+            // ใช้ ExpandableInfoCard ที่แยกไฟล์แล้ว
+            const ExpandableInfoCard(
               icon: Icons.description,
               title: 'เกี่ยวกับแอปพลิเคชัน',
               content: 'แอปพลิเคชันนี้พัฒนาด้วย Flutter เพื่อให้ผู้ใช้งานสามารถเข้าถึงข้อมูลและบริการต่างๆ ได้อย่างสะดวกและรวดเร็ว โดยมุ่งเน้นการใช้งานที่ง่ายและประสิทธิภาพที่ดี',
-              isExpanded: _isAppInfoExpanded,
-              onTap: () {
-                setState(() {
-                  _isAppInfoExpanded = !_isAppInfoExpanded;
-                });
-              },
             ),
-
             const SizedBox(height: 20),
 
-            _buildExpandableInfoCard(
+            const ExpandableInfoCard(
               icon: Icons.developer_mode,
               title: 'ผู้พัฒนา',
-              content: 'พัฒนาโดยทีมนักศึกษาสาขาวิทยาการคอมพิวเตอร์ที่กำลังศึกษาด้านการพัฒนาแอปพลิเคชันมือถือ โดยการใช้เทคโนโลยี Flutter และ Dart ในการสร้างแอปพลิเคชัน',
-              isExpanded: _isDeveloperExpanded,
-              onTap: () {
-                setState(() {
-                  _isDeveloperExpanded = !_isDeveloperExpanded;
-                });
-              },
+              content: 'พัฒนาโดยทีมงานผู้เชี่ยวชาญด้านการพัฒนาแอปพลิเคชันมือถือ ที่มีประสบการณ์ในการใช้เทคโนโลยี Flutter และ Dart ในการสร้างแอปพลิเคชันที่มีคุณภาพสูง',
             ),
-
             const SizedBox(height: 20),
 
-            _buildExpandableInfoCard(
+            const ExpandableInfoCard(
               icon: Icons.contact_mail,
               title: 'ติดต่อเรา',
               content: 'หากมีข้อสงสัยหรือต้องการความช่วยเหลือ สามารถติดต่อเราได้ผ่าน Contact ที่แอปพลิเคชัน หรือส่งข้อความถึงทีมงานของเราได้ตลอดเวลา เราพร้อมให้บริการและแก้ไขปัญหาต่างๆ',
-              isExpanded: _isContactExpanded,
-              onTap: () {
-                setState(() {
-                  _isContactExpanded = !_isContactExpanded;
-                });
-              },
             ),
-
             const SizedBox(height: 30),
 
-            // Additional Info Cards
+            // Additional Info Cards (ใช้ฟังก์ชันเดิม)
             Row(
               children: [
                 Expanded(
@@ -159,7 +130,6 @@ class _AboutScreenState extends State<AboutScreen> {
                 ),
               ],
             ),
-
             const SizedBox(height: 15),
 
             Row(
@@ -181,7 +151,6 @@ class _AboutScreenState extends State<AboutScreen> {
                 ),
               ],
             ),
-
             const SizedBox(height: 40),
 
             // Copyright
@@ -201,103 +170,7 @@ class _AboutScreenState extends State<AboutScreen> {
     );
   }
 
-  Widget _buildExpandableInfoCard({
-    required IconData icon,
-    required String title,
-    required String content,
-    required bool isExpanded,
-    required VoidCallback onTap,
-  }) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(15),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.1),
-            blurRadius: 10,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Column(
-        children: [
-          InkWell(
-            onTap: onTap,
-            borderRadius: BorderRadius.circular(10),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: Colors.blue[50],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Icon(
-                    icon,
-                    color: Colors.blue[600],
-                    size: 24,
-                  ),
-                ),
-                const SizedBox(width: 15),
-                Expanded(
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
-                  ),
-                ),
-                AnimatedRotation(
-                  turns: isExpanded ? 0.5 : 0,
-                  duration: const Duration(milliseconds: 300),
-                  child: Icon(
-                    Icons.keyboard_arrow_down,
-                    color: Colors.grey[600],
-                    size: 24,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          AnimatedCrossFade(
-            duration: const Duration(milliseconds: 300),
-            crossFadeState: isExpanded 
-              ? CrossFadeState.showSecond 
-              : CrossFadeState.showFirst,
-            firstChild: const SizedBox.shrink(),
-            secondChild: Container(
-              width: double.infinity,
-              margin: const EdgeInsets.only(top: 15),
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.grey[50],
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Colors.grey.withOpacity(0.2),
-                ),
-              ),
-              child: Text(
-                content,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey[700],
-                  height: 1.5,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
+  // ฟังก์ชันเดิมที่เหลือ - ไม่เปลี่ยนแปลง
   Widget _buildFeatureCard({
     required IconData icon,
     required String title,
